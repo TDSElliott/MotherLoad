50019e44815754d87743099506b46b138f2f2504
macko <macko@192.168.2.21>
2017-01-17 14:17:19 -0500

    Culling is done

--- MotherLoad/src/io/github/tdselliott/ml/control/LandControl.java
+++ MotherLoad/src/io/github/tdselliott/ml/control/LandControl.java
@@ -7,6 +7,11 @@ package io.github.tdselliott.ml.control;
 
 import com.almasb.ents.AbstractControl;
 import com.almasb.ents.Entity;
+import com.almasb.fxgl.app.FXGL;
+import com.almasb.fxgl.app.ServiceType;
+import com.almasb.fxgl.asset.AssetLoader;
+import com.almasb.fxgl.entity.EntityView;
+import com.almasb.fxgl.entity.component.MainViewComponent;
 import com.almasb.fxgl.entity.component.PositionComponent;
 import java.util.ArrayList;
 import javafx.geometry.Point2D;
@@ -18,14 +23,21 @@ import javafx.geometry.Point2D;
 public class LandControl extends AbstractControl {
 
     public int Tier;
+    public boolean Mined = false;
+
+    static {
+    }
+
+    @Override
+    public void onAdded(Entity entity) {
+    }
     
     public LandControl(int x) {
         Tier = x;
     }
-    
+
     @Override
     public void onUpdate(Entity entity, double d) {
-        
     }
-    
+
 }
