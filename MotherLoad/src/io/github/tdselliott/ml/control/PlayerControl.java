747e99438f5af19af7fba0a079d6ebb333d022ea
Tyler Elliott <tyler.ds.elliott@gmail.com>
2017-01-18 22:50:17 -0500

    Updating for Music

--- MotherLoad/src/io/github/tdselliott/ml/control/PlayerControl.java
+++ MotherLoad/src/io/github/tdselliott/ml/control/PlayerControl.java
@@ -1,30 +1,8 @@
-/*
- * The MIT License
- *
- * Copyright 2016 Mackenzie G.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package io.github.tdselliott.ml.control;
 
 import com.almasb.ents.AbstractControl;
 import com.almasb.ents.Entity;
+import static com.almasb.fxgl.app.FXGL.getAudioPlayer;
 import com.almasb.fxgl.entity.component.PositionComponent;
 import io.github.tdselliott.ml.MotherLoadApp;
 import javafx.geometry.Point2D;
@@ -221,6 +199,7 @@ public class PlayerControl extends AbstractControl {
                             MotherLoadApp.ground[arrX - 1][arrY].removeFromWorld();
                             //MotherLoadApp.ctrLand[arrX - 1][arrY].Mined = true;
                             MotherLoadApp.arrTier[arrX - 1][arrY] = -1;
+                            getAudioPlayer().playSound("Dig.wav");
                         }
                     }
                 }
@@ -234,6 +213,7 @@ public class PlayerControl extends AbstractControl {
                             MotherLoadApp.ground[arrX + 1][arrY].removeFromWorld();
                             //MotherLoadApp.ctrLand[arrX + 1][arrY].Mined = true;
                             MotherLoadApp.arrTier[arrX + 1][arrY] = -1;
+                            getAudioPlayer().playSound("Dig.wav");
                         }
                     }
                 }
